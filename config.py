3990ffb39f7ce6acb0f2b91055c76b2b056aca7a
Mehdi RAFENNE <mehdi.rafenne@gmail.com>
2020-04-23 23:27:30 +0300

    refactor: deleted venv folder from Git

--- config.py
+++ config.py
@@ -6,9 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
-
 
 # Database URI
-SQLALCHEMY_DATABASE_URI = 'postgres://USERNAME:PASSWORD@HOST:5432/DB_NAME'
+SQLALCHEMY_DATABASE_URI = 'postgres://farem:552XQJ77@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
