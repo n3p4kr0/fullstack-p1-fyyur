6240f18552cf61433029b5143041f31b150b6f22
Mehdi RAFENNE <mehdi.rafenne@gmail.com>
2020-04-23 18:35:41 +0300

    fix: models replaced in app.py instead of separate file due to bugs

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import FlaskForm
 from forms import *
+#from models import *
 import config as cfg
 import sys
-from models import *
+
 from filters import format_datetime
 #----------------------------------------------------------------------------#
 # App Config.
@@ -26,14 +27,56 @@ moment = Moment(app)
 # Connect to a local postgresql database
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-# Migration
-migrate = Migrate(app, db)
-
+migrate = Migrate(app, db, compare_type=True)
 
 #----------------------------------------------------------------------------#
 # Models (imported from models.py)
 #----------------------------------------------------------------------------#
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    website = db.Column(db.String(120), nullable=True)
+    genres = db.Column(db.ARRAY(db.String), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=True)
+    shows = db.relationship('Show', backref='venue', lazy=True)
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500), nullable=True)
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.ARRAY(db.String), nullable=False)
+    website = db.Column(db.String(120), nullable=True)
+    # TODO:: Change the default image
+    image_link = db.Column(db.String(500), nullable=False, default='https://images.unsplash.com/photo-1534294668821-28a3054f4256?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80')
+    facebook_link = db.Column(db.String(120), nullable=True)
+    shows = db.relationship('Show', backref='artist', lazy=True)
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500), nullable=True)
+
+
+class Show(db.Model):
+  __tablename__ = 'Show'
+
+  id = db.Column(db.Integer, primary_key=True)
+  start_time = db.Column(db.DateTime, nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+  image_link = db.Column(db.String(500), nullable=False, default='https://images.unsplash.com/photo-1459058537932-d95b3e068690?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80')
 
 #----------------------------------------------------------------------------#
 # Filters (imported from filters.py)
